ac3aa201525705ae03fb4d2d913d5638fb2314fa
ErickGabriel2023 <erickgabriel85431@gmail.com>
2024-09-05 07:47:23 -0300

    quarto commit: reparo no menu hamburguer

--- src/menuHanburgue.js
+++ src/menuHanburgue.js
@@ -2,20 +2,24 @@ const hamburger = document.getElementById('hamburger');
 const containerMenu = document.getElementById('container_menu');
 const closeBtn = document.getElementById('close-btn');
 const overlay = document.getElementById('overlay')
+const body = document.querySelector('body')
 
 
 
 hamburger.addEventListener('click', () => {
     containerMenu.classList.toggle('show');
     overlay.classList.toggle('show')
+    body.classList.toggle('remove-overflow')
 });
 
 closeBtn.addEventListener('click', () => {
     containerMenu.classList.remove('show');
     overlay.classList.remove('show')
+    
 });
 
 overlay.addEventListener('click', () => {
     containerMenu.classList.remove('show')
     overlay.classList.remove('show')
+    body.classList.remove('remove-overflow')
 })
